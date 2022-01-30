ca22b381e19a237391325b442c31b38c119faf29
aleksandarskrbic <skrbic.alexa@gmail.com>
2022-01-30 18:19:12 +0100

    Simple tcp server

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -15,8 +16,16 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, _)) => {}
-                Err(error) => println!("Failed to establish a connection {}", error),
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(error) => println!("Failed to read from connection: {}", error),
+                    }
+                }
+                Err(error) => println!("Failed to establish a connection: {}", error),
             }
         }
     }
